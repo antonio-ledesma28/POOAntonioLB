9522909e5db839377d8f05b22ff1fb323d7a6041
antonio-ledesma28 <54130633+antonio-ledesma28@users.noreply.github.com>
2020-02-12 00:41:20 -0600

    Tarea

---
+++ computadora.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package computadora;
+
+/**
+ *
+ * @author anton
+ */
+public class computadora {
+private String marca;
+private String procesador;
+private String sistemaOperativo;
+private int numDeVentiladores;
+private int cantidadDeRam;
+private String numDeSerie;
+private int elementosEntradas;
+private boolean portatil;
+    public computadora (){
+    }
+ public void procesarInformacion (){
+ }
+ public void acualizarSistema(){
+ }
+ public void mostrarResultados(){
+ }
+ public void apagar(){
+     
+ }
+    public void encenderPc(){
+        
+    }
+}
